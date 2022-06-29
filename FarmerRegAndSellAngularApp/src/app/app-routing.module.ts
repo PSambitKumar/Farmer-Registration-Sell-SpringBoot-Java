6f1aa715eba7718f49853db7d7387d6a80c3fe21
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-06-29 11:22:29 +0530

    Farmer Registration and Sell

--- FarmerRegAndSellAngularApp/src/app/app-routing.module.ts
+++ FarmerRegAndSellAngularApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FarmerRegistrationComponent} from "./component/farmer-registration/farmer-registration.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "farmerRegistration", component : FarmerRegistrationComponent},
+  {path : "", redirectTo : "farmerRegistration", pathMatch : "full" ,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FarmerRegAndSellAngularApp/src/app/app.module.ts
+++ FarmerRegAndSellAngularApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FarmerRegistrationComponent } from './component/farmer-registration/farmer-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FarmerRegistrationComponent
   ],
   imports: [
     BrowserModule,
